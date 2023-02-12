80eb5a4df1de805a9e21042202419b59e35cc939
Alexander <Alexander_alatorre1993@hotmail.com>
2023-02-11 18:51:40 -0600

    finished web components course

--- app.js
+++ app.js
@@ -5,11 +5,45 @@ template.innerHTML = `
         h3 {
             color: blue
         }
+
+        .user-card {
+            font-family: 'Arial', sans-serif;
+            background: #f4f4f4;
+            width: 500px;
+            display: grid;
+            grid-template-columns: 1fr 2fr;
+            grid-gap: 10px;
+            margin-bottom: 15px;
+            border-bottom: darkorchid 5px solid;
+        }
+    
+        .user-card img {
+            width: 100%;
+        }
+    
+        .user-card button {
+            cursor: pointer;
+            background: darkorchid;
+            color: #fff;
+            border: 0;
+            border-radius: 5px;
+            padding: 5px 10px;
+        }
     </style>
+
     <div class="user-card">
         <img class="image" />
         <div>
             <h3 class="tertiary-title"></h3>
+            <div class="info">
+
+                <!-- This text will show up in the slot -->
+                    <p><slot name="email" /></p>
+                    <p><slot name="phone" /></p>
+                <!-- This text will show up in the slot -->
+
+            </div>
+            <button id="toggle-info">Hide Info</button>
         </div>
     </div>
 `;
@@ -17,6 +51,9 @@ template.innerHTML = `
 class UserCard extends HTMLElement {
   constructor() {
     super();
+
+    // state
+    this.showInfo = true;
 
     // create shadow DOM
     this.attachShadow({ mode: "open" });
@@ -31,6 +68,37 @@ class UserCard extends HTMLElement {
 
     // attach an image to the template
     this.shadowRoot.querySelector(".image").src = this.getAttribute("avatar");
+  }
+
+  toggleInfo() {
+    this.showInfo = !this.showInfo;
+
+    const info = this.shadowRoot.querySelector(".info");
+    const toggleBtn = this.shadowRoot.querySelector("#toggle-info");
+
+    if (this.showInfo) {
+      info.style.display = "block";
+      toggleBtn.innerText = "Hide info";
+      return;
+    }
+
+    info.style.display = "none";
+    toggleBtn.innerText = "Show info";
+  }
+
+  // adds event listener
+  // manipulate the DOM with event listener to hide elements
+  connectedCallback() {
+    const infoButton = this.shadowRoot.querySelector("#toggle-info");
+
+    infoButton.addEventListener("click", () => this.toggleInfo());
+  }
+
+  // removes the event listener
+  disconnectedCallback() {
+    const infoButton = this.shadowRoot.querySelector("#toggle-info");
+
+    infoButton.removeEventListener();
   }
 }
 
